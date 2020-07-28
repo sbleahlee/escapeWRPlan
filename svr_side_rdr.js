c3eed39b2ad88d2e2db59c4a80e37b65492561c5
sbleahlee <ewhaelle@gmail.co>
2020-07-28 20:43:32 +0200

    5th_final

--- svr_side_rdr.js
+++ svr_side_rdr.js
@@ -2,8 +2,8 @@ var http = require('http');
 var fs = require('fs');
 var qs = require('querystring')
 const paserurl = require('url');
-const { notEqual } = require('assert');
-const { isUndefined } = require('util');
+//const { notEqual } = require('assert');
+//const { isUndefined } = require('util');
 
 
 const userInfo = [
@@ -23,72 +23,30 @@ const init_func = function (req, res) {
         console.log('PATHNAME : ', pathname);
 
         if(pathname == '/'){     
-
+            // localserver:8080/ → login.html로 직행
             res.end(fs.readFileSync(__dirname + url + 'login.html'));
-
-        } 
-        else if (pathname == '/index.html') 
-        { 
-
-            const queryObject = paserurl.parse(url, true).query; //parsing URL. object-tpe return
-
-            var name = 'guest'
-            var title = 'No Title'
-
-            if(queryObject.name)
-                name = queryObject.name
-            if(queryObject.title)
-                title = queryObject.title
-            
-            console.log('queryObject : ', queryObject);
-
-            //res.end(fs.readFileSync(__dirname + pathname));
-            fs.readFile(__dirname + pathname, 'UTF-8',
-                function(err, data){
-                    var conv_data = data.replace(/#name#/g, name).replace(/#title#/g, title);
-                    res.writeHead(200, {'Content-Type' : 'text/html'});
-                    res.write(conv_data);
-                    res.end();
-                }
-            )
-
-        } 
-        else if ( req.url == '/login')
-        {
-            url = '/login.html';
-            res.end(fs.readFileSync(__dirname + url));
-
         } else {
             console.log('그리고 아무것도 일어나지 않았따-');
         }
-    } else if (method == 'POST'){
-        
+    } else if (method == 'POST'){       
+
         var cli_data = '';
 
         req.on('data', function(data){
             cli_data += data; //데이터가 잘려서 들어올 수 있다는 점.
             console.log('Data Event');
         });
+        
+        if (pathname == '/index.html') {
+            req.on('end', function(data){
 
-        if(pathname == '/'){
-            res.end(fs.readFileSync(__dirname + url + 'login.html'));
-        } 
-        else if (pathname == '/index.html') {
-            req.on('end', function(){
                 console.log('End Event');
-                console.log('type은 ', typeof(cli_data));
-    
-                var post = qs.parse(cli_data);            
-                console.log('JSON이 parse했어 : ', post);
-    
+                    
+                var post = qs.parse(cli_data);  
+
                 var loginuser = userInfo.find(el => el['id'] == post.id);
-                console.log(loginuser); // { id: '조이', pw: '유이', name: '루이' }
-                console.log(typeof(loginuser));
-                console.log(pathname);
-    
-                //* else로 왜 계속 빠질까
-                //if(typeof(loginsuser)=="undefined"|| loginuser == null || loginuser == ""){
-                if(typeof(loginsuser)!="undefined"){ 
+
+                if(loginuser!==undefined){   
 
                     fs.readFile(__dirname + pathname, 'UTF-8',
                     function(err, data){
@@ -98,10 +56,8 @@ const init_func = function (req, res) {
                         res.end();
                         }
                     )                         
-                    console.log('이구간통과');
                     
                 }else{
-                    console.log('진짜else로왔다는증거');
                     res.end(
                         `
                         <!DOCTYPE html>
@@ -111,8 +67,8 @@ const init_func = function (req, res) {
                             <title>Error</title>
                           </head>
                           <body>
-                            <h1> Alert가 undefined </h1>
-                            <p> Alert창 닫기 시 다시 로그인창으로 </p>
+                            <h1> 당신은 멤버가 아니에요 </h1>
+                            <p> 아님아님 </p>
                           </body>
                         </html> 
                         `
@@ -127,12 +83,6 @@ const init_func = function (req, res) {
                 res.end(``)
                 */    
             });
-        }
-        else if ( req.url == '/login')
-        {
-            url = '/login.html';
-            res.end(fs.readFileSync(__dirname + url));
-
         } else {
             console.log('그리고 아무것도 일어나지 않았따-');
         }   
